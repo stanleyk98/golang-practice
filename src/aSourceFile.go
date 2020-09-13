535ab5e88e2520be8372adc2efdae210e8b8ceee
stanleyk <stanleyk98@gmail.com>
2020-09-13 15:21:34 +0900

    1st~

---
+++ src/aSourceFile.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("처음만난 Go go  잘해보자그래 ~~ \n")
+}
---
+++ src/packageNotUsed.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	_ "os"
+)
+
+func main() {
+	fmt.Println("import 한 패키지는 사용해야한다~! 단, vs 에서는 알아서 패키지를 제거해줌")
+}
