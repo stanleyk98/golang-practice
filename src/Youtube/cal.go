c28c26bd52aed8dd4cb2f4f8265dcb2a23749576
stanleyk150009 <stanleyk@dreamus.io>
2021-02-01 20:22:27 +0900

    20210201

---
+++ src/Youtube/cal.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var a int
+	// var b int
+	// a = 3
+	// b = 4
+
+	a := 3
+	b := 4
+	c := "Stanley"
+	d := "good"
+	e := 3.1415
+
+	fmt.Println(a + b)
+	fmt.Println(c + d)
+	fmt.Printf("a+e = %v\n", a+b)
+	fmt.Printf("원주율은 : %v\n", e)
+
+}
